41a0c8a39140afe48d729046c07c8d3ef64b2ae2
John Martin <tekbuddha@gmail.com>
2015-10-21 15:20:41 -0400

    Whole lotta catch up.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,24 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+ChefSpec::Coverage.start!
+
+RSpec.configure do |config|
+  # Output results in color
+  # config.color = true
+
+  # Configure output format
+  # 'documentation' is best used for local development
+  # config.formatter = 'documentation'
+  # 'progress' is best used by a build server/agent
+  # config.formatter = 'progress'
+
+  # Specify the Chef log_level (default: :warn)
+  # config.log_level = :debug
+
+  # Specify the path to a local JSON file with Ohai data
+  # config.path = 'spec/fauxhai/linux-redhat65.json'
+  # config.path = 'spec/fauxhai/windows-server2012.json'
+
+  config.cookbook_path = File.expand_path('../../../', __FILE__)
+end
